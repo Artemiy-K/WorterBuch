8a5660f8aecf875935ba937f6e6db8778253f52c
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-30 15:23:35 +0200

    create a seit

--- learnwordseasy/admin.py
+++ learnwordseasy/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Words, Category
 
-# Register your models here.
+
+class WordsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title1', 'title2', 'example2', 'category')
+    list_display_links = ('id', 'title1', 'title2')
+    search_fields = ('title1', 'title2', 'example2')
+    list_filter = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title1')
+    list_display_links = ('id', 'title1')
+    search_fields = ('title1',)
+
+
+admin.site.register(Words, WordsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- learnwordseasy/models.py
+++ learnwordseasy/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Words(models.Model):
+    title1 = models.CharField(max_length=150, verbose_name='Слово')
+    title2 = models.CharField(max_length=150, verbose_name='Перевод', null=True)
+    example1 = models.TextField(blank=True, verbose_name='Предлложение 1')
+    example2 = models.TextField(blank=True, verbose_name='Предлложение 2')
+    example3 = models.TextField(blank=True, verbose_name='Предлложение 3')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title1
+
+    class Meta:
+        verbose_name = 'Слово'
+        verbose_name_plural = 'Слова'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title1 = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title1
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title1']
