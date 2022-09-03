5b7e7960615d3492fc07fe40403d920c052bd645
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-09-03 13:10:09 +0200

    make a design of seit

--- learnwordseasy/views.py
+++ learnwordseasy/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Category, Words
 
 
 def index(request):
-    return HttpResponse("DAROVA ALES OKK")
+    words = Words.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'learnwordseasy/index.html', {
+        'words': words,
+        'title': 'список слов',
+        'categories': categories,
+    })
